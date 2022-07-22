5114e977b9df5fc45acf55037572711d514f3c34
heiyuyu <72803402+heiyuyui@users.noreply.github.com>
2022-07-22 14:59:57 +0800

    v.0

---
+++ README.md
@@ -0,0 +1,80 @@
+# vue3-dashboard-record
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+VUE_APP_API=http://localhost:8080/
+VUE_APP_PATH=jasonlee2357
+
+
+//---------------------------
+vue3-dashboard-record 設定說明
+
+vue3-dashboard-record
+https://github.com/Wcc723/vue3-dashboard-record
+
+修改 .env檔案，將內容改成 
+
+VUE_APP_API=https://vue3-course-api.hexschool.io/
+VUE_APP_PATH=jasonlee2357
+
+VUE_APP_PATH 可以換成你的API路徑
+
+//---------
+cd vue3-dashboard-record-master
+
+npm install
+
+npm run serve
+
+首頁 http://localhost:8080/#/
+前台介面 http://localhost:8080/#/user/cart
+
+後台介面 
+登入頁：http://localhost:8080/#/login 
+產品：http://localhost:8080/#/dashboard/products
+訂單：http://localhost:8080/#/dashboard/orders
+優惠劵：http://localhost:8080/#/dashboard/coupons
+
+六角學院 Vue 課程練習 API 申請
+https://vue3-course-api.hexschool.io/ 
+
+API 文件
+https://github.com/hexschool/vue3-course-api-wiki/wiki
+
+//---  API 資料取得 網址示範 -----
+取得商品列表_all
+https://vue3-course-api.hexschool.io/api/jasonlee2357/products/all
+
+取得購物車列表
+https://vue3-course-api.hexschool.io/api/jasonlee2357/cart
+
+取得訂單列表
+https://vue3-course-api.hexschool.io/api/jasonlee2357/orders?page=1
+
+六角學院 Vue 課程練習 API 申請
+
+https://vue-course-api.hexschool.io
+jasonlee2357@gmail.com
+BhNSg3UABptH3ST
+      
---
+++ src/components/LinePayModal.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="modal fade" id="productModal" tabindex="-1" role="dialog"
+       aria-labelledby="exampleModalLabel" aria-hidden="true" ref="modal">
+    <div class="modal-dialog modal-xl" role="document">
+      <div class="modal-content border-0">
+        <div class="modal-header text-black">
+          <h5 class="modal-title" id="exampleModalLabel">
+            <span>LINE Pay QRCode</span>
+          </h5>
+          <button type="button" class="btn-close btn-outline-secondary"
+                  data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div>
+          <img src="/img/logo.82b9c7a5.png" class="rounded mx-auto d-block" />
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-outline-secondary"
+                  @click="$emit('update-order', tempOrder)">取消</button>
+          <button type="button" class="btn btn-primary"
+                  data-bs-dismiss="modal" @click="$emit('update-checkout')">確認
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import modalMixin from '@/mixins/modalMixin';
+
+export default {
+  name: 'LinePayModal',
+  props: {
+    order: {
+      type: Object,
+      default() { return {}; },
+    },
+  },
+  data() {
+    return {
+      status: {},
+      modal: '',
+      tempOrder: {},
+      isPaid: false,
+    };
+  },
+  methods: {
+  },
+  emits: ['update-checkout'],
+  mixins: [modalMixin],
+  inject: ['emitter'],
+  watch: {
+    order() {
+      this.tempOrder = this.order;
+      this.isPaid = this.tempOrder.is_paid;
+    },
+  },
+};
+</script>
